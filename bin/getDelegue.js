ff6909dbaf0aa0b602015a27af1f65372c082efa
flow <simon.florian.toulouse@gmail.com>
2017-04-12 17:56:23 +0200

    add assesseur handling in extraction script

--- bin/getDelegue.js
+++ bin/getDelegue.js
@@ -16,25 +16,87 @@ async function iterate() {
   console.log('new iteration of all requests');
   var cursor = 0;
   var datas;
+  var assesseurRedis;
   var delegueRedis;
+  var personInfo;
 
   // Iterate redis SCAN
   for(;;) {
-    [cursor, datas] = await redis.scanAsync(cursor, 'MATCH', `${config.redisPrefix}*:*:*`, 'COUNT', '99');
-
+    [cursor, datas] = await redis.scanAsync(cursor, 'MATCH', `${config.redisPrefix}assesseurs:*:*:*`, 'COUNT', '99');
     for (var i = 0; i < datas.length; i++) {
       var data = datas[i];
-      if ((delegueRedis = await redis.getAsync(`${data}`))) {
-        var delegueJson = (JSON.parse(delegueRedis));
+      if ((assesseurRedis = await redis.getAsync(`${data}`))) {
+        if ((personInfo = await redis.getAsync(`jlm2017:bureaux:${assesseurRedis}`))) {
+          var assesseurJSON = (JSON.parse(personInfo));
+          if (!jsonLog[assesseurJSON.insee]) {
+            jsonLog[assesseurJSON.insee] = {};
+          }
+          if (!jsonLog[assesseurJSON.insee][assesseurJSON.bureaux]) {
+            jsonLog[assesseurJSON.insee][assesseurJSON.bureaux] = {};
+          }
+          assesseurJSON.second_tour = assesseurJSON.second_tour ? 'oui' : 'non';
+          jsonLog[assesseurJSON.insee][assesseurJSON.bureaux][assesseurJSON.role] = assesseurJSON;
+          continue;
+        }
+      }
+    }
+    if (cursor == '0') {
+      break;
+    }
+    var assesseurListe = [];
 
-        if (!jsonLog[delegueJson.insee]) {
-          jsonLog[delegueJson.insee] = {};
+    for (var insee in jsonLog) {
+      for (var bureau in jsonLog[insee]) {
+        for (var role in jsonLog[insee][bureau]) {
+          assesseurListe.push(jsonLog[insee][bureau][role]);
         }
-        if (!jsonLog[delegueJson.insee][delegueJson.bur]) {
-          jsonLog[delegueJson.insee][delegueJson.bur] = {};
+      }
+    }
+
+    var columns = {
+      email: 'email',
+      first_name: 'first_name',
+      last_name: 'last_name',
+      phone: 'phone',
+      date: 'date',
+      zipcode: 'zipcode',
+      address1: 'address1',
+      address2: 'address2',
+      ville: 'ville_adresse',
+      commune: 'commune',
+      insee: 'insee',
+      bureaux: 'bureaux',
+      bureau_list: 'bureau_list',
+      numero_list: 'numero_list',
+      role: 'role',
+      second_tour: 'second_tour'
+    };
+
+    stringify(assesseurListe, { header: true, columns: columns , quotedString: true, quotedEmpty: true}, function(err, output){
+      var fileName = 'logs/' + Date.now() + '-extract-assesseurs.csv';
+
+      fs.writeFileSync(fileName, output);
+    });
+  }
+
+  jsonLog = {};
+
+  for(;;) {
+    [cursor, datas] = await redis.scanAsync(cursor, 'MATCH', `${config.redisPrefix}delegues:*:*`, 'COUNT', '99');
+
+    for (var j = 0; j < datas.length; j++) {
+      var data2 = datas[j];
+      if ((delegueRedis = await redis.getAsync(`${data2}`))) {
+        if ((personInfo = await redis.getAsync(`jlm2017:bureaux:${delegueRedis}`))) {
+          var delegueJson = (JSON.parse(personInfo));
+
+          if (!jsonLog[delegueJson.insee]) {
+            jsonLog[delegueJson.insee] = {};
+          }
+          delegueJson.second_tour = delegueJson.second_tour ? 'oui' : 'non';
+          jsonLog[delegueJson.insee][delegueJson.bur] = delegueJson;
+          continue;
         }
-        jsonLog[delegueJson.insee][delegueJson.bur][delegueJson.role] = delegueJson;
-        continue;
       }
     }
     if (cursor == '0') {
@@ -44,35 +106,36 @@ async function iterate() {
 
   var delegueListe = [];
 
-  for (var insee in jsonLog) {
-    for (var bureau in jsonLog[insee]) {
-      for (var role in jsonLog[insee][bureau]) {
-        delegueListe.push(jsonLog[insee][bureau][role]);
-      }
+  for (var insee2 in jsonLog) {
+    for (var bureau2 in jsonLog[insee2]) {
+      delegueListe.push(jsonLog[insee2][bureau2]);
     }
   }
 
-  var columns = {
-    insee: 'insee',
-    bur: 'bureau',
+  var columns2 = {
+    email: 'email',
     first_name: 'first_name',
     last_name: 'last_name',
-    email: 'email',
     phone: 'phone',
-    date: 'birthday',
-    subscribtionDate: 'subscribtionDate',
+    date: 'date',
+    zipcode: 'zipcode',
     address1: 'address1',
     address2: 'address2',
-    role: 'role'
+    ville: 'ville_adresse',
+    commune: 'commune',
+    insee: 'insee',
+    bureaux: 'bureaux',
+    bureau_list: 'bureau_list',
+    numero_list: 'numero_list',
+    role: 'role',
+    second_tour: 'second_tour'
   };
 
-  stringify(delegueListe, { header: true, columns: columns , quotedString: true, quotedEmpty: true}, function(err, output){
-    var fileName = Date.now() + '-extract-delegue.csv';
+  stringify(delegueListe, { header: true, columns: columns2 , quotedString: true, quotedEmpty: true}, function(err, output){
+    var fileName = 'logs/' + Date.now() + '-extract-delegues.csv';
 
     fs.writeFileSync(fileName, output);
   });
-
-
 }
 
 iterate()
